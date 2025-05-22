69885e332a2f8c54c9828e45c9cecc0cc17cc4b6
brandon-bwoni <bwonibrandon@gmail.com>
2025-05-22 07:30:55 +0200

    initial commit of a simple agent bot

---
+++ simple-agent-bot/agent_bot.py
@@ -0,0 +1,8 @@
+from typing import TypedDict, List
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+
+load_dotenv()
