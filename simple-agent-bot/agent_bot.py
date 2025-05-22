f6129233ca8451549514fee03a29b08a0a5463e6
brandon-bwoni <bwonibrandon@gmail.com>
2025-05-22 07:59:53 +0200

    revoke llm and interact with AI using a graph

--- simple-agent-bot/agent_bot.py
+++ simple-agent-bot/agent_bot.py
@@ -6,3 +6,30 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+    
+    
+llm = ChatOpenAI(model="gpt-4o")
+
+# Define node within a function
+def process(state: AgentState) -> AgentState:
+    """Node function to invoke the LLM."""
+    response = llm.invoke(state["messages"])
+    print(f"\nAI: {response.content}")
+    return state
+
+
+# Define the state graph
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+
+user_input = input("Enter: ")
+while user_input.lower() != "exit":
+    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
